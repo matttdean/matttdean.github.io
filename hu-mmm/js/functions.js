789bc7773e8bdd94228b1af61eb3ef30ee02075d
Matthew Dean <matttdeann@gmail.com>
2018-04-02 09:18:34 -0500

    Progress

--- hu-mmm/js/functions.js
+++ hu-mmm/js/functions.js
@@ -2,9 +2,27 @@ $(window).scroll(function(){
 	var wScroll = $(this).scrollTop();
 
 	$('.brain').css({
-		'transform' : 'translate(0px,-'+ wScroll /4 +'%)'
+		'transform' : 'translate(0px, -'+ wScroll /4 +'%)'
 	})
 	$('.blue-bottom').css({
 		'transform' : 'translate(0px,-'+ wScroll /32 +'%)'
 	})
+	$('.moon').css({
+		'transform' : 'translate(0px, -'+ wScroll /26 +'%)'
+	})
+	$('.brain-clouds').css({
+		'transform' : 'translate(0px, '+ wScroll /64 +'%)'
+	})
+	$('.dark-blue-bottom').css({
+		'transform' : 'translate(0px, -'+ wScroll /64 +'%)'
+	})
+	$('.darkest-blue-bottom').css({
+		'transform' : 'translate(0px, -'+ wScroll /53 +'%)'
+	})
+	$('.white-bottom').css({
+		'transform' : 'translate(0px, -'+ wScroll /42 +'%)'
+	})
+
 });
+
+	

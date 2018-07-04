2d62b953a12428c6d2be45e061bcfd7894123f93
Matthew Dean <matttdeann@gmail.com>
2018-07-04 12:54:57 -0500

    added mobile nav

--- avast-site/js/scripts.js
+++ avast-site/js/scripts.js
@@ -1,8 +1,10 @@
+  var portCount = 1;
 
 $(function(){
   $(".blueButton").click(function() {
   $(".blue-page").toggleClass("translated");
   $(".blue-clip").toggleClass("clip-translate");
+  $(".blue-grid").toggleClass("slideIn");
   // $(".space").toggleClass("fade");
   });
 });
@@ -22,6 +24,101 @@ $(function(){
 $(function(){
   $(".purpleButton").click(function() {
   $(".purple-page").toggleClass("opacity");
+  });
+});
+
+$(function(){
+  $(".mobile-nav-button").click(function() {
+  $(".mobile-nav").toggleClass("translated");
+  $(".line-top").toggleClass("rotate-top");
+  $(".line-middle").toggleClass("fade-middle");
+  $(".line-bottom").toggleClass("rotate-bottom");
+  });
+});
+
+$(function(){
+  $("#port1").click(function() {
+    portCount = 1;
+  // $(".modal-box").slideDown( "slow" );
+  $(".modal-box").removeClass("isVisible");
+  $("#m-1").toggleClass("isVisible");
+  });
+  $("#port2").click(function() {
+    portCount = 2;
+  // $(".modal-box").slideDown( "slow" );
+  $(".modal-box").removeClass("isVisible");
+  $("#m-2").toggleClass("isVisible");
+  });
+  $("#port3").click(function() {
+    portCount = 3;
+  // $(".modal-box").slideDown( "slow" );
+  $(".modal-box").removeClass("isVisible");
+  $("#m-3").toggleClass("isVisible");
+  });
+  $("#port4").click(function() {
+    portCount = 4;
+  // $(".modal-box").slideDown( "slow" );
+  $(".modal-box").removeClass("isVisible");
+  $("#m-4").toggleClass("isVisible");
+  });
+  $("#port5").click(function() {
+    portCount = 5;
+  // $(".modal-box").slideDown( "slow" );
+  $(".modal-box").removeClass("isVisible");
+  $("#m-5").toggleClass("isVisible");
+  });
+  $("#port6").click(function() {
+    portCount = 6;
+  // $(".modal-box").slideDown( "slow" );
+  $(".modal-box").removeClass("isVisible");
+  $("#m-6").toggleClass("isVisible");
+  });
+
+});
+
+
+// $(function(){
+//   for (i=1; i<7; i++) {
+//   $("#port" + i + "").click(function() {
+//     portCount = i;
+//   // $(".modal-box").slideDown( "slow" );
+//   $(".modal-box").removeClass("isVisible");
+//   $("#m-" + i + "").toggleClass("isVisible");
+//   });
+// }
+// });
+
+
+$(function(){
+  $(".close").click(function() {
+  // $(".modal-box").slideDown( "slow" );
+  $(".modal-box").removeClass("isVisible");
+  });
+});
+
+$(function(){
+  $(".next").click(function() {
+    if (portCount < 5 ) {
+    portCount++;}
+    else {
+      portCount = 1;
+    }
+  // $(".modal-box").slideDown( "slow" );
+  $(".modal-box").removeClass("isVisible");
+  $("#m-" + portCount + "").addClass("isVisible");
+  });
+});
+
+$(function(){
+  $(".prev").click(function() {
+    if (portCount > 1 ) {
+    portCount--;}
+    else {
+      portCount = 6;
+    }
+  // $(".modal-box").slideDown( "slow" );
+  $(".modal-box").removeClass("isVisible");
+  $("#m-" + portCount + "").addClass("isVisible");
   });
 });
 
@@ -67,6 +164,11 @@ $("#scroll-top").click(function() {
     }, 1000);
 });
 
+$(".port-item").click(function() {
+    $('.blue-page').animate({
+        scrollTop: 0
+    }, 1000);
+});
 $(document).scroll(function() {
   var y = $(this).scrollTop();
   if (y > 800) {

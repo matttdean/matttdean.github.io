45b16bccb6cf03b10932a074dc8def8c832113fb
Matthew Dean <matttdeann@gmail.com>
2018-07-21 00:53:59 -0500

    upload

--- mountain-site/js/scripts.js
+++ mountain-site/js/scripts.js
@@ -2,8 +2,14 @@ function simpleParallax(intensity, element) {
         $(window).scroll(function() {
             var scrollTop = $(window).scrollTop();
             var imgPos = scrollTop / intensity + 'px';
-            element.css('transform', 'translateY(' + imgPos + ')');
+            element.css('transform', 'translateY(-' + imgPos + ')');
         });
     }
+var m1 = $(".m-1");
+var m2 = $(".m-2");
+var m3 = $(".m-3");
+var m4 = $(".m-4");
 
-    simpleParallax(4, ".m-1");
+simpleParallax(5, m1);
+simpleParallax(10, m2);
+simpleParallax(20, m3);

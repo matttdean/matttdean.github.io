92fc3c4fb53a9b410253480bda77f9c7f96c2bd5
Matthew Dean <matttdeann@gmail.com>
2019-03-25 16:29:08 -0400

    lots of stuff

--- brilliance/js/scripts.js
+++ brilliance/js/scripts.js
@@ -4,6 +4,32 @@ $(function() {
     });
 });
 
+$(function() {
+    $( "#button" ).click(function() {
+        $( "#hidden-container-mobile" ).toggle(1000);
+    });
+});
+
 
 $('.counter').countUp();
     
+
+$(window).scroll(function () {
+    var s = $(window).scrollTop(),
+          d = $(document).height(),
+          c = $(window).height();
+          scrollPercent = (s / (d-c)) * 100;
+          var position = scrollPercent;
+    if (scrollPercent < 3) {
+        $(".nav-mobile").attr('style', 'background-color: transparent');
+    }
+    else {
+        $(".nav-mobile").attr('style', 'background-color: #FFF')
+    }
+    if (scrollPercent > 95) {
+        $(".nav-mobile").attr('style', 'display:none');
+    }
+     $(".progress-bar").attr('style', 'width:' + position + '%');
+     
+  
+  });
